07efba6418941c571b80bf87115fff23369ecd70
Samuel Sogbesan <ss3222@bath.ac.uk>
2020-05-20 11:55:39 +0100

    Updates

---
+++ precache-manifest.b955a3223334a12af902b0482c7b7c85.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5710e1687df9d4ad49e8b3564832adcf",
+    "url": "/Project/index.html"
+  },
+  {
+    "revision": "c0289e74419b2bb6eda0",
+    "url": "/Project/static/css/main.b23f068f.chunk.css"
+  },
+  {
+    "revision": "e9eb1af03f77c8fccf7b",
+    "url": "/Project/static/js/2.c96bc2a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Project/static/js/2.c96bc2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0289e74419b2bb6eda0",
+    "url": "/Project/static/js/main.13f4f65f.chunk.js"
+  },
+  {
+    "revision": "745a4102df042e96279a",
+    "url": "/Project/static/js/runtime-main.04e53f3f.js"
+  },
+  {
+    "revision": "c28a68b1bdd512e8db0eca06744d5193",
+    "url": "/Project/static/media/BalooPaaji2-Bold.c28a68b1.ttf"
+  },
+  {
+    "revision": "9b538d73efd7c492a32292dbc63b79fc",
+    "url": "/Project/static/media/BalooPaaji2-Regular.9b538d73.ttf"
+  },
+  {
+    "revision": "b98587efd67d32f5a37fdb074aac1aaa",
+    "url": "/Project/static/media/cv.b98587ef.pdf"
+  },
+  {
+    "revision": "d2aee0831eeddc9ebba0e0adfd5787f1",
+    "url": "/Project/static/media/cv.d2aee083.svg"
+  },
+  {
+    "revision": "deda33aea2bfa9216df4b9abc2d17698",
+    "url": "/Project/static/media/email.deda33ae.svg"
+  },
+  {
+    "revision": "0c0d0a7bc23460d052f6b87639c86d46",
+    "url": "/Project/static/media/github.0c0d0a7b.svg"
+  },
+  {
+    "revision": "7175b6995ab59b80e6fbc85cb670de84",
+    "url": "/Project/static/media/home-icon.7175b699.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/Project/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "c62395ee894da101fe8c5427fe1f0b3f",
+    "url": "/Project/static/media/profile-image.c62395ee.gif"
+  },
+  {
+    "revision": "9bda81de16f62a6f77210f82b32e21e0",
+    "url": "/Project/static/media/rocket.9bda81de.svg"
+  }
+]);
