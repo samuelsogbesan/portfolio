5c41af97394f981ce6b39fd6eee225ca7530b541
Samuel Sogbesan <ss3222@bath.ac.uk>
2020-05-21 11:32:55 +0100

    Build App

--- react-app/build/service-worker.js
+++ react-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Project/precache-manifest.fe85ef21899093c840b4a97bc741b7b7.js"
+  "/precache-manifest.ea6f4ceed6f511ac7ac3bd8373cdc1c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
